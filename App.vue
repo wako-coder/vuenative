c5e2efbe11a6d487f324cb86ddc9731ddfd9a4fe
Wakoya Kumsa <waqo1234567@gmail.com>
2021-08-21 21:50:13 +0300

    cool project

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press me!" @press="exclaim" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+</style>
